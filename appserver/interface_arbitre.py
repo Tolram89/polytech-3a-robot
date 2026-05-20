ecdb2f761e42d4df89d6fabb377e066a51e01d2c
evan marlot <evanmarlot89@gmail.com>
2026-05-20 08:30:37 +0200

    creation d'une fenetre

---
+++ appserver/interface_arbitre.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+class FenetreArbitre(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle("Exemple de Fenêtre")
+        self.setGeometry(100, 100, 800, 600) 
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    
+    fenetre = FenetreArbitre()
+    fenetre.show()
+    
+    sys.exit(app.exec())
