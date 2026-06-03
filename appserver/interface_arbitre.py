b68eea7ceec33741a3f43b2ff5ac5e241e48ba25
evan marlot <evanmarlot89@gmail.com>
2026-06-03 10:51:10 +0200

    fonction calculer score fini

--- appserver/interface_arbitre.py
+++ appserver/interface_arbitre.py
@@ -1,26 +1,36 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow
-from PyQt6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QListWidget, QTextEdit, QTableWidget, QLabel, QPushButton
-from PyQt6.QtWidgets import QGroupBox 
+from PyQt6.QtWidgets import (
+    QWidget,
+    QHBoxLayout,
+    QVBoxLayout,
+    QListWidget,
+    QTextEdit,
+    QTableWidget,
+    QLabel,
+    QPushButton,
+)
+from PyQt6.QtWidgets import QGroupBox
+
 
 class FenetreArbitre(QMainWindow):
     def __init__(self):
         super().__init__()
-        
+
         self.setWindowTitle("Interface arbitre")
-        self.setGeometry(100, 100, 800, 600) 
-        
+        self.setGeometry(100, 100, 800, 600)
+
         widget_central = QWidget()
         self.setCentralWidget(widget_central)
 
         layout_principal_horizontal = QHBoxLayout()
         colonne_gauche_verticale = QVBoxLayout()
         colonne_droite_verticale = QVBoxLayout()
-        
-        #à enlever plus tard
+
+        # à enlever plus tard
         simulation_button = QPushButton("simulation")
         simulation_button.clicked.connect(self.simuler_reception)
-        
+
         self.list_robot = QListWidget()
         boite_robots = QGroupBox("Robots en piste")
         layout_interne_robot = QVBoxLayout()
@@ -28,22 +38,19 @@ class FenetreArbitre(QMainWindow):
         layout_interne_robot.addWidget(simulation_button)
         boite_robots.setLayout(layout_interne_robot)
 
-        
         self.score = QTableWidget()
         boite_score = QGroupBox("Score")
         layout_interne_score = QVBoxLayout()
         layout_interne_score.addWidget(self.score)
         boite_score.setLayout(layout_interne_score)
-        
-        
+
         self.log = QTextEdit()
         self.log.setReadOnly(True)
         boite_log = QGroupBox("Log")
         layout_interne_log = QVBoxLayout()
         layout_interne_log.addWidget(self.log)
         boite_log.setLayout(layout_interne_log)
-        
-        
+
         colonne_gauche_verticale.addWidget(boite_robots)
         colonne_droite_verticale.addWidget(boite_score)
         colonne_droite_verticale.addWidget(boite_log)
@@ -52,12 +59,13 @@ class FenetreArbitre(QMainWindow):
         layout_principal_horizontal.addLayout(colonne_droite_verticale)
 
         widget_central.setLayout(layout_principal_horizontal)
-        
-    def simuler_reception(self) :
+
+    def simuler_reception(self):
         self.list_robot.addItem("Robot ABC123")
         self.log.append("[10:42] Robot ABC123 a fait le mouvement ALU")
-    
-def textToDictionnaire(fichier) :
+
+
+def textToDictionnaire(fichier):
     regles = {}
     couleur_actuelle = None
     with open(fichier, "r", encoding="utf-8") as file:
@@ -68,39 +76,43 @@ def textToDictionnaire(fichier) :
             line = line.strip()
             if line.startswith("["):
                 couleur_actuelle = line[1]
-                regles[couleur_actuelle]={}
-            elif "=" in line :
+                regles[couleur_actuelle] = {}
+            elif "=" in line:
                 morceaux = line.split("=")
                 regles[couleur_actuelle][morceaux[0]] = int(morceaux[1].strip())
     print(regles)
     return regles
 
-def calculerScore(json, regle) :
+
+def calculerScore(json, regle):
     col = json["col"]
     arm = json["arm"]
-    score=0
-    for key in regle[col] :
-        if ' ' in arm :
-            regle_cherche = arm.replace(' ', '+') 
-            if regle_cherche == key :
-                score+=regle[col][key]
-        if ',' in key :
-            arm_propre = arm.replace(' ', ',')
-            if arm_propre == key:
-                score+=regle[col][key]
-        #elif 
-        #à continuer
-    
+    exp = json["exp"]
+
+    score = 0
+    arm_propre = arm.replace(" ", "+")
+    exp_propre = exp.replace(" ", "+")
+
+    for key in regle[col]:
+
+        if arm_propre == key or exp_propre == key :
+            score += regle[col][key]
+
+        elif "," in key:
+            key_propre = key.split(",")
+            if arm_propre in key_propre:
+                score += regle[col][key]
+            if exp_propre in key_propre:
+                score += regle[col][key]
+
     return score
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    
+
     fenetre = FenetreArbitre()
     fenetre.show()
-    
-    
+
     textToDictionnaire("appserver/exemple.battle")
     sys.exit(app.exec())
-
