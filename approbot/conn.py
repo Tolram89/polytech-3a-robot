ec829a268c46b6fec0bad07ac44ef0b3db89aded
anrmd <arthur.normand714@gmail.com>
2026-06-03 12:08:16 +0200

    scan complet du reseau pour trouver la liste des ip

--- approbot/conn.py
+++ approbot/conn.py
@@ -1,26 +1,34 @@
 from martypy import Marty
 import deplacement
 
-print("Entrer l'ip du Marty")
-ip = input()
 
-if not ip:
-    #ip part defaut
-    ip = "192.168.0.113"
-    print("ip par defaut : ", ip)
+def connection(ip):
+    try:
+        my_marty = Marty("wifi", ip)
+        print("connexion etablie")
+        return my_marty
+    except Exception as e:
+        print(f"erreur : {e}")
+        return None
+    
 
-
-try:
-    my_marty = Marty("wifi", ip)
-    print("connexion etablie")
-    
-    deplacement.avancer(my_marty, 10)
-    
-    my_marty.dance()    
-    
+def deco(my_marty):
     my_marty.close()
     print("deconnexion")
-    
-except Exception as e:
-    print(f"erreure : {e}")
+        
+
+
+if __name__ == '__main__':
+    ip = input("Entrer l'ip du Marty : ")
+    if not ip:
+        ip = "192.168.1.6"
+        print("ip par defaut :", ip)
+
+    robot = connection(ip)
+    if robot:
+        deplacement.avancer(robot, 2)
+        print("test robot")
+        deco(robot)
+
+
 
